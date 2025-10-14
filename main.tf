546b316a149ce275058ebd3c3373ad0e9061bb1d
chidamv <38436900+chidamv@users.noreply.github.com>
2025-10-14 10:18:27 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -37,7 +37,7 @@ module "blog_sg" {
   ingress_rules = ["https-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
-  egress_rules = ["all.all"]
+  egress_rules = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"] 
 }
 
