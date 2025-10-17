c192f951e13d1c735acd56828831de087a888637
chidamv <38436900+chidamv@users.noreply.github.com>
2025-10-16 19:38:50 -0700

    load balancer

--- main.tf
+++ main.tf
@@ -19,23 +19,18 @@ data "aws_vpc" "default" {
 }
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
-
   name = "dev"
   cidr = "10.0.0.0/16"
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  
+
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-
 
   tags = {
     Terraform = "true"
     Environment = "dev"
   }
 }
-
-
 
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
@@ -46,6 +41,51 @@ resource "aws_instance" "blog" {
   subnet_id = module.blog_vpc.public_subnets[0]
   tags = {
     Name = "HelloWorld"
+  }
+}
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name    = "blog-alb"
+  vpc_id  = module.blog_vpc.vpc_id
+  subnets = module.blog_vpc.public_subnets
+  seceurity_groups = module.blog_sg.security_group_id
+
+ 
+  listeners = {
+    ex-http-https-redirect = {
+      port     = 80
+      protocol = "HTTP"
+      redirect = {
+        port        = "443"
+        protocol    = "HTTPS"
+        status_code = "HTTP_301"
+      }
+    }
+    ex-https = {
+      port            = 443
+      protocol        = "HTTPS"
+      certificate_arn = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
+
+      forward = {
+        target_group_key = "ex-instance"
+      }
+    }
+  }
+
+  target_groups = {
+    ex-instance = {
+      name_prefix      = "blog-"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+      target_id        = aws_instance.blog.id
+    }
+  }
+
+  tags = {
+    Environment = "Dev"
+    Project     = "Example"
   }
 }
 
